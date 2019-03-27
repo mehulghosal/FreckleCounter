73a4c9ad1d0cfe2ebf443f5cc1a94f91098e4bb7
mehulghosal <mehul.ghosal@gmail.com>
2019-03-27 17:39:56 -0400

    added gitignore

--- freckle.py
+++ freckle.py
@@ -1,7 +1,11 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
-img = cv.imread('freckles1.jpg')
-cv.imshow('image', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+img = cv2.imread('freckles1.jpg', 0)
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
