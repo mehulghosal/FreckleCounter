03b9a1e2676c03ad238cf2aef1060aeeb91c670c
mehulghosal <mehul.ghosal@gmail.com>
2019-03-27 18:26:15 -0400

    face detection works

--- freckle.py
+++ freckle.py
@@ -1,12 +1,24 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('freckles1.jpg', 0)
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# uses haar face detection built into cv2
+# returns rectangle with coordinates of recognized face 
+def faceDetect(img):
+	haar_cascade_face = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+	# as scaleFactor gets smaller, the smaller the rectangle gets
+	faces_rects = haar_cascade_face.detectMultiScale(img, scaleFactor = 1.005, minNeighbors = 5);
+	print(faces_rects)
+	for (x,y,w,h) in faces_rects:
+		cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	display(img)
+	return faces_rects
 
-haar_cascade_face = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+# displays given image
+def display(img):
+	cv2.imshow('image', img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
-
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+	img = cv2.imread('freckles1.jpg')
+	faceDetect(img)
