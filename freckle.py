14f1ea46a8ebc930538915179cbf3614a511ecb8
mehulghosal <mehul.ghosal@gmail.com>
2019-03-27 17:42:50 -0400

    Updated .gitignore

--- freckle.py
+++ freckle.py
@@ -4,6 +4,7 @@ import numpy as np
 img = cv2.imread('freckles1.jpg', 0)
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+haar_cascade_face = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
 
 cv2.imshow('image', img)
